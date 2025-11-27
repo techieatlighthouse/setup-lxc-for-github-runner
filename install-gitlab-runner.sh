94946123d1496b6b0b306adb9835e6125693bff8
data-mechanic-oak-journey <avinash.negi.tech@gmail.com>
2025-11-27 10:11:39 +0530

    Create install-gitlab-runner.sh

---
+++ install-gitlab-runner.sh
@@ -0,0 +1,6 @@
+sudo curl -L --output /usr/local/bin/gitlab-runner "https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64"
+sudo chmod +x /usr/local/bin/gitlab-runner
+sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+sudo rm -f /home/gitlab-runner/.bash_logout
+sudo systemctl start gitlab-runner 
